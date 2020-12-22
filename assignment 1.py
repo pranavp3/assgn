d051abf1757a6a934b4326b5d398f5be97ced3c3
pranavp3 <73294545+pranavp3@users.noreply.github.com>
2020-12-22 17:33:33 +0530

    Update assignment 1.py

--- assignment 1.py
+++ assignment 1.py
@@ -1,22 +1,20 @@
 
 # taking examples from assignment file
 responses='''I really like the show because it is thought provoking and i like shows that make me think
-The cast.
-Nothing
-It is the most complex and original idea I have ever seen or heard of. Also, because it delves into the topic of human emotions, but in an artificial way, if I were to be punny and serious all at once. From what I have seen of the show, I believe that we, as human beings, can compare ourselves to the androids, because we can definitely relate to them.
-it has many twists and I like that
-It's unpredictable so you are left wanting more'''
+             The cast.
+             Nothing
+             It is the most complex and original idea I have ever seen or heard of. Also, because it delves into the topic of human emotions, but in an artificial way, if I were to be punny and serious all at once. From what I have seen of the show, I believe that we, as human beings, can compare ourselves to the androids, because we can definitely relate to them.
+             it has many twists and I like that
+             It's unpredictable so you are left wanting more'''
 
 import nltk
 import re
-
-#converting to sentences
-sent=nltk.sent_tokenize(responses)
-
 from nltk.stem import WordNetLemmatizer
 let=WordNetLemmatizer()
 from nltk.corpus import stopwords
 
+#converting to sentences
+sent=nltk.sent_tokenize(responses)
 
 #data cleaning and keyword generation
 keywords=[]
